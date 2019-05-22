80bc8eb1ad45cbf81522a32fbef1598648b7db6c
Dwake5 <wakeling101@hotmail.com>
2019-05-22 13:55:56 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Your code here
+// 1. Default arguments
+const saturdayFun = (event = 'roller-skate') =>
+{
+  return `This Saturday, I want to ${event}!`  
+}
+
+// 2. Default params again
+const mondayWork = (event = 'go to the office') =>
+{
+    return `This Monday, I will ${event}.`  
+}
+
+// 3. Scope chain
+const wrapAdjective = (style="*") => {
+    return (adjective="special") => {
+      return `You are ${style}${adjective}${style}!`
+    }
+}
+
+// 4. Define an object called Calculator
+const Calculator = {
+    add: (a,b) => {
+      return a + b;
+    },
+    subtract: (a,b) => {
+        return a - b;
+    },
+    multiply: (a,b) => {
+        return a * b;
+    },
+    divide: (a,b) => {
+        return a / b;
+    }
+}
+
+const actionApplyer = (start, ray) => {
+    let a = start
+  
+    for (let i = 0; i < ray.length; i++ ){
+      a = ray[i](a)
+    }
+  
+    return a
+  }
